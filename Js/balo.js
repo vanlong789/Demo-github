7daa965ef563abfae227b1e231c150b4b2b716cc
nguyenvanlich <buoivip199@gmail.com>
2021-11-05 23:09:06 +0700

    upload

--- Js/balo.js
+++ Js/balo.js
@@ -1,22 +1,22 @@
-var clickLogin= document.querySelector('.js-hearder-seach');
-var clickRemove = document.querySelector('.js-modal-list__icon');
-var deleteRemove=document.querySelector('.js-modal-inner');
+// var clickLogin= document.querySelector('.js-hearder-seach');
+// var clickRemove = document.querySelector('.js-modal-list__icon');
+// var deleteRemove=document.querySelector('.js-modal-inner');
 
-var onModal = document.querySelector('.modal');
+// var onModal = document.querySelector('.modal');
 
-clickLogin.onclick = function(e){
-    onModal.classList.add('open');
-};
+// clickLogin.onclick = function(e){
+//     onModal.classList.add('open');
+// };
 
-clickRemove.onclick = function(e){
-    onModal.classList.remove('open');
-};
-onModal.onclick = function(e){
-    onModal.classList.remove('open');
-}
-deleteRemove.onclick = function(event){
-    event.stopPropagation()
-}
+// clickRemove.onclick = function(e){
+//     onModal.classList.remove('open');
+// };
+// onModal.onclick = function(e){
+//     onModal.classList.remove('open');
+// }
+// deleteRemove.onclick = function(event){
+//     event.stopPropagation()
+// }
 
 
 
